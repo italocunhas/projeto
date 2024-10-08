b5122b30c53c1db5a61bedc47da72f41119e7d60
italocunhas <126425722+italocunhas@users.noreply.github.com>
2024-10-07 23:10:05 -0300

    comd

---
+++ src/components/basicos/Primeiro.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Primeiro(){
+    const msg = 'Seja bem vindo(a) ao primeiro componente';
+    return (
+        <div>
+            <h2>Primeiro Componente</h2>
+            <p>{msg}</p>
+        </div>
+    )
+}
