8d07373ec04a5c276d52b21af431bec8706e86e8
italocunhas <126425722+italocunhas@users.noreply.github.com>
2024-10-15 18:07:30 -0300

    comm

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import { createRoot } from 'react-dom/client';
 import './index.css'
-import Segundo from './components/basicos/Primeiro';
+import Primeiro from './components/basicos/Primeiro';
 import React from 'react';
-
+import ComParametro from './components/basicos/ComParametro';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <>
+  <div id="app">
     <h1>Olá React!</h1>
-    <Segundo />
+    <Primeiro></Primeiro>
+    <ComParametro 
+      titulo=" Situação do aluno "
+      aluno= "Pedro " nota = { 9.3 }/>
+      <ComParametro 
+      titulo=" Situação do aluno "
+      aluno="Maria " nota = { 4.3 }/>
+  </div>
   </>
 );
-a = "q"
